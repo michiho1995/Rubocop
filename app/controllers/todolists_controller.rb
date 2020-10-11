b04848d2dc3c15957638404475770e4ce8cf5b1e
EC2 Default User <ec2-user@ip-172-31-76-216.ec2.internal>
2020-10-11 07:02:03 +0000

    [finish]4-7

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -36,8 +36,8 @@ class TodolistsController < ApplicationController
   private
   # ストロングパラメータ
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
-  
+
 end
 
