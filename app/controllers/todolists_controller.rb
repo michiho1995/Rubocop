0e23cdb968527bb701a90e55f743a5fd58064359
EC2 Default User <ec2-user@ip-172-31-76-216.ec2.internal>
2020-11-04 10:48:06 +0000

    [ouyoukadai,finish]1-5

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -1,19 +1,18 @@
 class TodolistsController < ApplicationController
-
   def new
     @list = List.new
   end
 
-   def create
+  def create
     # １. データを新規登録するためのインスタンス作成
     list = List.new(list_params)
     # ２. データをデータベースに保存するためのsaveメソッド実行
     list.save
     # ３. トップ画面へリダイレクト
-     # redirect_to '/top' を削除して、以下コードに変更
+    # redirect_to '/top' を削除して、以下コードに変更
     # 詳細画面へリダイレクト
     redirect_to todolist_path(list.id)
-   end
+  end
 
   def index
     @lists = List.all
@@ -34,19 +33,15 @@ class TodolistsController < ApplicationController
   end
 
   def destroy
-    list = List.find(params[:id])  #データ（レコード）を一件取得
-    list.destroy  #データ（レコード）を消去
-    redirect_to todolists_path #投稿一覧画面へリダイレクト
-
-
-
+    list = List.find(params[:id]) # データ（レコード）を一件取得
+    list.destroy # データ（レコード）を消去
+    redirect_to todolists_path # 投稿一覧画面へリダイレクト
   end
 
   private
+
   # ストロングパラメータ
   def list_params
     params.require(:list).permit(:title, :body, :image)
   end
-
 end
-
